14be73260fd43db76d4044700b1885379c195732
Guillaume Grossetie <ggrossetie@gmail.com>
2018-10-26 11:15:00 +0200

    Improve IncludeResolver
    - read from the filesystem (if the file exists)
    - otherwise read from the classpath
    - else return an empty string

--- src/main/java/org/asciidoctor/GraalVMIncludeResolver.java
+++ src/main/java/org/asciidoctor/GraalVMIncludeResolver.java
@@ -1,23 +1,33 @@
 package org.asciidoctor;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GraalVMIncludeResolver {
   public String read(String path) throws IOException, URISyntaxException {
-    URL url = this.getClass().getClassLoader().getResource(path);
-    if (url != null) {
-      List<String> lines = Files.readAllLines(Paths.get(url.toURI()), StandardCharsets.UTF_8);
-      return String.join("\n", lines);
+    Path filePath = Paths.get(path);
+    List<String> lines;
+    File file = filePath.toFile();
+    if (file.exists()) {
+      lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
     } else {
-      List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
-      return String.join("\n", lines);
+      Path fileName = filePath.getFileName();
+      URL url = this.getClass().getClassLoader().getResource(fileName.toString());
+      if (url != null) {
+        lines = Files.readAllLines(Paths.get(url.toURI()), StandardCharsets.UTF_8);
+      } else {
+        lines = new ArrayList<>();
+      }
     }
+    return String.join("\n", lines);
   }
 
   public String pwd() {
